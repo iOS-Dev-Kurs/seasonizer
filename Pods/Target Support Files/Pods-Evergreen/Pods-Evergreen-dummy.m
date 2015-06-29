fb40e5f15803e2e528cc6839b9b3ee96173c8103
Nils Fischer <n.fischer@viwid.com>
2015-06-29 13:25:12 +0200

    added podfile and evergreen submodule

---
+++ Pods/Target Support Files/Pods-Evergreen/Pods-Evergreen-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Evergreen : NSObject
+@end
+@implementation PodsDummy_Pods_Evergreen
+@end
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Evergreen
+#define COCOAPODS_POD_AVAILABLE_Evergreen
+#define COCOAPODS_VERSION_MAJOR_Evergreen 0
+#define COCOAPODS_VERSION_MINOR_Evergreen 5
+#define COCOAPODS_VERSION_PATCH_Evergreen 0
+
