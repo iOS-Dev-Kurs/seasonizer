e6c496b45b57c708277cfcf5f743204d3019471a
felix <felixmeissner@me.com>
2016-05-22 23:59:36 +0200

    seasonizer; seltsame declaration Fehler

--- Seasonizer/AccessoryListViewController.swift
+++ Seasonizer/AccessoryListViewController.swift
@@ -20,18 +20,41 @@ class AccessoryListViewController: UITableViewController {
     // MARK: User Interaction
     
     // TODO: Implement `prepareForSegue(_:sender:)` to set `selectedAccessory` when the "selectedAccessory" Segue is performed.
-    /*
-     HINT: Obtain the selected index path with:
-     
+    // HINT: Obtain the selected index path with:
+ 
+    //
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int{
+        return 1
+    }
+    // 1
+    override func tableView(tableView: UITableView, numberOfRowsInSection section:Int)->Int{
+        return accessories?.count ?? 0
+    }
+    // 2
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath:NSIndexPath)->UITableViewCell{
+        let accessory = accessories![indexPath.row]
+        let cell = tableView.dequeueReusableCellWithIdentifier("accessoryCell",forIndexPath:indexPath); cell.textLabel?.text = accessory.title; cell.imageView?.image = accessory.image
+        return cell
+    }
+    // 3
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?){
+        //
         guard let indexPath = tableView.indexPathForSelectedRow else {
             return
         }
-
-    */
+        selectedAccessory = accessories![indexPath.row]
+        //
+    }
+    
+    
 
 }
 
 
 // MARK: - Table View Data Source
+
+
+
+
 
 // TODO: Implement the `UITableViewDataSource` protocol.
--- Seasonizer/CanvasViewController.swift
+++ Seasonizer/CanvasViewController.swift
@@ -22,6 +22,7 @@ class CanvasViewController: UIViewController, UINavigationControllerDelegate, UI
     
     /// Eine `UIImageView`, die das Foto anzeigt.
     @IBOutlet weak var photoImageView: UIImageView!
+    
     /// Eine View, die _über_ der `photoImageView` positioniert ist und die Accessories anzeigt.
     @IBOutlet weak var accessoryOverlayView: UIView!
     
@@ -120,37 +121,39 @@ class CanvasViewController: UIViewController, UINavigationControllerDelegate, UI
     
     
     // TODO: Implement `prepareForSegue(_:sender:)` to pass `allAccessories` on to `AccessoryListViewController`.
-    /*
-     HINT: The `AccessoryListViewController` should be embedded in a `UINavigationController`:
+    // HINT: The `AccessoryListViewController` should be embedded in a `UINavigationController`:
      
-         guard let accessoryListViewController = (segue.destinationViewController as? UINavigationController)?.topViewController as? AccessoryListViewController else {
+     guard let accessoryListViewController = (segue.destinationViewController as? UINavigationController)?.topViewController as? AccessoryListViewController else {
             return
-         }
-    */
+     }
     
     
     // TODO: Implement an `@IBAction func unwindToCanvas(segue: UIStoryboardSegue)` Unwing Segue that the `AccessoryListViewController` can exit to.
+    @IBAction func unwindToCanvas(segue: UIStoryboardSegue){
+        
+     //no implementation
+    }
+
     
     // TODO: For the "selectedAccessory" segue, obtain the selected accessory and add it to the canvas.
-    /*
-     HINTS:
+    
+    // HINTS:
      
-     - The `AccessoryListViewController` is the segue's `sourceViewController`:
+    // - The `AccessoryListViewController` is the segue's `sourceViewController`:
      
-         guard let accessoryListViewController = segue.sourceViewController as? AccessoryListViewController,
-                selectedAccessory = accessoryListViewController.selectedAccessory else {
-            return
-         }
+     guard let accessoryListViewController = segue.sourceViewController as? AccessoryListViewController,
+            selectedAccessory = accessoryListViewController.selectedAccessory else {
+        return
+     }
      
-     - Create an `AccessoryView` from the `selectedAccessory` and set its initial position:
+     //- Create an `AccessoryView` from the `selectedAccessory` and set its initial position:
      
-        let accessoryView = AccessoryView(accessory: selectedAccessory)
-        accessoryView.center = accessoryOverlayView.convertPoint(accessoryOverlayView.center, fromView: accessoryOverlayView.superview)
+    let accessoryView = AccessoryView(accessory: selectedAccessory)
+    accessoryView.center = accessoryOverlayView.convertPoint(accessoryOverlayView.center, fromView: accessoryOverlayView.superview)
 
-     - Finally, call the `addAccessoryView(_:)` Method implemented above:
+     //- Finally, call the `addAccessoryView(_:)` Method implemented above:
      
-        self.addAccessoryView(accessoryView)
-    */
+    self.addAccessoryView(accessoryView)
 
 }
 
